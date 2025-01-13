b3f805a5cdbbdedfe45a8df36811397849a4c2de
tdnsyh <102989771+tdnsyh@users.noreply.github.com>
2025-01-13 23:55:08 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,9 @@ Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
+Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+
 
 Route::middleware(['auth'])->group(function () {
     //admin
